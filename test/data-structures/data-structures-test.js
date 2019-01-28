32d3b1836f5def47308feeda21b3e29743c45f7e
Louis345 <jt125845@gmail.com>
2019-01-27 19:01:16 -0500

    added one test verfiy things are working properly

--- test/data-structures/data-structures-test.js
+++ test/data-structures/data-structures-test.js
@@ -3,9 +3,12 @@ const chai = require('chai');
 const expect = chai.expect;
 chai.use(require('sinon-chai'));
 const {
-  isPalindrome
+  createAnArray
 } = require('../../exercises/01-data-structures/data-structures.js');
 
 describe('Data structures', () => {
-  it('should return an array with values inside', () => {});
+  it('should return an array with three values', () => {
+    const newArr = createAnArray();
+    expect(newArr.length).to.equal(3);
+  });
 });
