179b9cdeafaeef223d97785b8570e9760003fc9d
Louis345 <jt125845@gmail.com>
2019-02-09 16:10:50 -0500

    added questions for data-structures review. Still need to add more questions. WIP

---
+++ exercises/03-data-structures-review/data.structures-review.js
@@ -0,0 +1,32 @@
+/**
+ *  Upper case the first letter in each array element
+ * @param  { array }
+ * @return {array}
+ * @example
+ * upperCaseEachElementInArray(['javascript','html','css']) // ['Javascript','HTML','CSS']
+ * upperCaseEachElementInArray(['Brad','Tom']) // ['Brad','Tom']
+ *
+ *
+ */
+
+function upperCaseEachElementInArray(array) {
+  /*** Dont forget to return an array with all elements being upper-cased */
+}
+
+/**
+ *  Use the Spread Operator to add additional values to an existing object using the second argument
+ * @param  {obj, string}
+ * @return {array}
+ * @example
+ * addMiddleNameToObject({firstName:'Tom',lastName:'Jerry'},'Harris') // {firstName:'Tom',MiddleName:'Harris', lastName:'Jerry'}
+ *
+ */
+
+/**
+ * Use split method to return an array
+ * @param  { string }
+ * @return { array }
+ * @example
+ */
+
+function stringToArrayConverter(str) {}
