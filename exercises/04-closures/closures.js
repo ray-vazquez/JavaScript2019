7c851174adf736249e5faa02974a49b2b49f627e
Matina Patsos <matinaspatsos@gmail.com>
2019-02-10 14:34:26 -0500

    Conditionals, closures and currying exercises

---
+++ exercises/04-closures/closures.js
@@ -0,0 +1,27 @@
+/**
+ * Use closures to complete this exercise.
+ * For those of you who are familar with object-orientened programming,
+ * this exercise will use a similar pattern with "setters" and "getters".
+ * @returns {Object} an object that has two methods. See comments below.
+ */
+const calculator = () => {
+  /**
+   * Create a private variable called "sum"
+   * @var {number}
+   */
+  /**
+   * Return an object that has two methods:
+   *
+   * 1. The first is a "setter" function that a.) accepts a parameter of type number and
+   *    b.) adds that number to the "sum" above.
+   * @param {number}
+   *
+   * 2. The second function is a "getter" function
+   * that should return the value of "sum" above.
+   * @returns {number} the value of sum
+   */
+};
+
+module.exports = {
+  calculator
+};
