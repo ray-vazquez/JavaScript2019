c2dbe20b743be977df13cc8ff05e1fdeae344380
Louis345 <jt125845@gmail.com>
2019-02-12 20:21:11 -0500

    code challenges for day 3

---
+++ exercises/05-challenges/challenges.js
@@ -0,0 +1,40 @@
+/**
+ * Check if a string (first argument, str) ends with the given target string (second argument, target).
+ *  @param  {string}
+ *  @return {string}
+ *
+ *  @example confirmEnding("Bastian", "n") true
+ *  @example confirmEnding("Connor", "n")  false
+ *
+ */
+
+function confirmEnding() {}
+/**
+ *  Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number.
+ *  @param  {string}
+ *  @return {string}
+ *
+ *  @example repeatStringNumTimes("*", 3)
+ *  @example repeatStringNumTimes("abc", 3) should return "abcabcabc"
+ *  @example repeatStringNumTimes("abc", -2) should return ""
+ */
+
+function repeatStringNumTimes() {}
+
+/**
+ *  Find the Longest Word in a String
+ *  @param {string}
+ *  @return {string}
+ *
+ *  @example findLongestWordLength("Google do a barrel roll")  should return 6
+ *  @example findLongestWordLength("May the force be with you") should return 5
+ *
+ */
+
+function findLongestWordLength() {}
+
+module.exports = {
+  findLongestWordLength,
+  confirmEnding,
+  repeatStringNumTimes
+};
