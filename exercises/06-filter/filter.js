bd064c51b8e2cc58d469cb6fc5d5b3b3739d89e9
Matina Patsos <matinaspatsos@gmail.com>
2019-02-19 20:15:17 -0500

    High Order Function Array exercises

---
+++ exercises/06-filter/filter.js
@@ -0,0 +1,34 @@
+/**
+ * Build your own filter function
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+ * @param {array} arr
+ * @param {function} callback
+ * @returns {mixed} a array of values with the values with some of the values removed
+ */
+const filter = (arr, callback) => {};
+
+/**
+ * Delete the matching user from an array of user objects
+ * @param {array} arr array of objects, where each object represents a user
+ * @param {number} id the user's id
+ * @returns {object} the array of user objects, but the user object with the matching id is removed
+ *
+ * @example
+ * const users = [{
+ *  id: 1024,
+ *  username: "smile134",
+ *  email: "smile134@example.com"
+ * },{
+ *  id: 1025,
+ *  username: "newyorkfarmer",
+ *  email: "johndoe@example.com"
+ * }]
+ * findUser(users, 1025);
+ * // [{ id: 1024, username:"smile134", email: "smile134@example.com" }]
+ */
+const deleteUser = (arr, id) => {};
+
+module.exports = {
+  filter,
+  deleteUser
+};
