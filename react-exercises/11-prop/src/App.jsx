2d580d7218c5f774e582afd114fe24b7b1f84198
Matina Patsos <mpatsos@deepbluecommunications.com>
2019-03-10 21:52:52 -0400

    Updating instructions for props

---
+++ react-exercises/11-prop/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+// Import IsUserLoggedIn here
+
+class App extends Component {
+  render() {
+    const Users = [
+      // Complete me
+    ];
+    return (
+      <div className="App">
+        {/* Add IsUserLoggedIn component here with props */}
+      </div>
+    );
+  }
+}
+
+export default App;
--- react-exercises/11-prop/src/App.test.js
+++ react-exercises/11-prop/src/App.test.js
@@ -1,47 +1,47 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import { shallow, mount } from 'enzyme';
-import App from './App';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { expect } from 'chai';
-import IsLoggedIn from './components/isLoggedIn';
+import ReactDOM from "react-dom";
+import React from "react";
+import { shallow, mount } from "enzyme";
+import App from "./App";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { expect } from "chai";
+import IsUserLoggedIn from "./components/isLoggedIn";
 
 Enzyme.configure({ adapter: new Adapter() });
-describe('Prop', () => {
+describe("Prop", () => {
   const Users = [
     {
-      name: 'Matina',
+      name: "Matina",
       isUserLoggedIn: false
     },
     {
-      name: 'Joe',
+      name: "Joe",
       isUserLoggedIn: true
     },
     {
-      name: 'Tom',
+      name: "Tom",
       isUserLoggedIn: true
     }
   ];
-  const wrapper = shallow(<IsLoggedIn Users={Users} />);
+  const wrapper = shallow(<IsUserLoggedIn Users={Users} />);
 
-  it('component renders the correct amount of users', () => {
+  it("component renders the correct amount of users", () => {
     wrapper.setProps({ Users: Users });
-    expect(wrapper.find('li').length).equal(6);
+    expect(wrapper.find("li").length).equal(6);
   });
-  it('renders the correct logged in status for a user', () => {
+  it("renders the correct logged in status for a user", () => {
     const Users = [
       {
-        name: 'James',
+        name: "James",
         isUserLoggedIn: false
       }
     ];
     wrapper.setProps({ Users: Users });
     expect(
       wrapper
-        .find('li')
+        .find("li")
         .at(1)
         .text()
-    ).equal('Logged Out');
+    ).equal("Logged Out");
   });
 });
