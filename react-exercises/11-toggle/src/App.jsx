3fd27c9abfd86e13799a066215d5895b1d90cb83
Ray <herschel.rey@gmail.com>
2019-03-17 10:02:02 -0400

    completed toggle exercise

--- react-exercises/11-toggle/src/App.jsx
+++ react-exercises/11-toggle/src/App.jsx
@@ -10,16 +10,22 @@ class App extends Component {
     showText: true
   };
 
-  toggle = () => {};
+  toggle = () => {
+    this.setState({
+      showText: !this.state.showText
+    })
+  };
 
   render() {
     return (
       <div className="container pt-4">
-        <button type="button" class="btn btn-primary btn-lg mb-2">
+
+        <button type="button" class="btn btn-primary btn-lg mb-2"
+         onClick={this.toggle}>
           Show / Hide Text
         </button>
-        {/* Hide the text here */}
-        <h1>Now you see me!</h1>
+        { this.state.showText && <h1>Now you see me!</h1>}
+  
       </div>
     );
   }
